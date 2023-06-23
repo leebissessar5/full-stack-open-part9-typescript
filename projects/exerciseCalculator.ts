3c9f53221401d7a19f42265bd1fdc43b4b0c8019
leebissessar5 <leebissessar@gmail.com>
2023-06-23 12:25:09 -0400

    scale rating between 1 to 5

--- projects/exerciseCalculator.ts
+++ projects/exerciseCalculator.ts
@@ -15,12 +15,12 @@ const calculateExercises = (dailyHours: number[], targetHours: number): Result =
   const trainingDays: number = dailyHours.filter((val) => val !== 0).length
   const totalHours: number = dailyHours.reduce((a, b) => a + b, 0)
   const average: number = totalHours / periodLength
-  const rating: number = (trainingDays / periodLength) * (totalHours / (targetHours * periodLength))
+  const rating: number = (trainingDays / periodLength) * (totalHours / (targetHours * periodLength)) * 5
   let ratingDescription: string = ""
 
-  if (rating >= 1) {
+  if (rating >= 4) {
     ratingDescription = 'Great job! You achieved your target.'
-  } else if (rating >= 0.5) {
+  } else if (rating >= 2) {
     ratingDescription = 'Not bad! You made progress towards your target.'
   } else {
     ratingDescription = 'Keep going! You need to work harder to reach your target.'
@@ -42,5 +42,5 @@ const calculateExercises = (dailyHours: number[], targetHours: number): Result =
 try {
   console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
 } catch (error) {
-  console.error(error.message);
+  console.error(error.message)
 }
