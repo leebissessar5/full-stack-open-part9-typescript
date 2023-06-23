76f3c5e2f3a1983bfc4221ad8c7e0d4f1599d848
leebissessar5 <leebissessar@gmail.com>
2023-06-23 12:14:54 -0400

    complete ex9.2

---
+++ projects/exerciseCalculator.ts
@@ -0,0 +1,46 @@
+interface Result { 
+    periodLength: number,
+    trainingDays: number,
+    success: boolean,
+    rating: number,
+    ratingDescription: string,
+    target: number,
+    average: number 
+}
+
+
+const calculateExercises = (dailyHours: number[], targetHours: number): Result => {
+
+  const periodLength: number = dailyHours.length
+  const trainingDays: number = dailyHours.filter((val) => val !== 0).length
+  const totalHours: number = dailyHours.reduce((a, b) => a + b, 0)
+  const average: number = totalHours / periodLength
+  const rating: number = (trainingDays / periodLength) * (totalHours / (targetHours * periodLength))
+  let ratingDescription: string = ""
+
+  if (rating >= 1) {
+    ratingDescription = 'Great job! You achieved your target.'
+  } else if (rating >= 0.5) {
+    ratingDescription = 'Not bad! You made progress towards your target.'
+  } else {
+    ratingDescription = 'Keep going! You need to work harder to reach your target.'
+  }
+
+  const result: Result = {
+    periodLength,
+    trainingDays,
+    success: trainingDays > 0,
+    rating,
+    ratingDescription,
+    target: targetHours,
+    average,
+  }
+
+  return result
+}
+
+try {
+  console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+} catch (error) {
+  console.error(error.message);
+}
