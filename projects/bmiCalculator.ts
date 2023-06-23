37b76571fbf2773a19c44de31f00290e2ca45275
leebissessar5 <leebissessar@gmail.com>
2023-06-23 12:21:43 -0400

    add commandline args to bmiCalculator

--- projects/bmiCalculator.ts
+++ projects/bmiCalculator.ts
@@ -1,5 +1,23 @@
-const calculateBmi = (height: number, weight: number): string => {
+interface BMIValues {
+    value1: number,
+    value2: number
+}
 
+const parseArguments = (args: string[]): BMIValues => {
+  if (args.length < 4) throw new Error('Not enough arguments')
+  if (args.length > 4) throw new Error('Too many arguments')
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      value1: Number(args[2]),
+      value2: Number(args[3]),
+    }
+  } else {
+    throw new Error('Provided values were not numbers!')
+  }
+}
+
+const calculateBmi = (height: number, weight: number): string => {
   const KG_TO_POUNDS = 2.204623
   const CM_TO_INCHES = 0.3937008
 
@@ -12,7 +30,7 @@ const calculateBmi = (height: number, weight: number): string => {
   const weightInPounds: number = weight * KG_TO_POUNDS
   const heightInInches: number = height * CM_TO_INCHES
 
-  const bmi: number = (weightInPounds / (heightInInches) ** 2) * 703
+  const bmi: number = (weightInPounds / heightInInches ** 2) * 703
 
   const UNDERWEIGHT_THRESHOLD = 18.5
   const NORMAL_THRESHOLD = 25
@@ -34,7 +52,12 @@ const calculateBmi = (height: number, weight: number): string => {
 }
 
 try {
-  console.log(calculateBmi(180, 74))
-} catch (error) {
-  console.error(error.message)
+  const { value1, value2 } = parseArguments(process.argv)
+  console.log(calculateBmi(value1, value2))
+} catch (error: unknown) {
+  let errorMessage = "Something bad happened."
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message
+  }
+  console.log(errorMessage)
 }
