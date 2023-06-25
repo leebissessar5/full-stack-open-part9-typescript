ebd715686c2b04ea2080143891d667cc1c19f102
leebissessar5 <leebissessar@gmail.com>
2023-06-24 21:14:01 -0400

    complete ex9.14

--- courseinfo/src/App.tsx
+++ courseinfo/src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+interface HeaderProps {
+  name: string
+}
 
-function App() {
+interface PartProps {
+  name: string,
+  exerciseCount: number
+}
+
+interface ContentProps {
+  parts: PartProps[]
+}
+
+const Header = (props: HeaderProps): JSX.Element => (
+  <>
+    <h1>{props.name}</h1>
+  </>
+);
+
+const Part = (props: PartProps): JSX.Element => (
+  <>
+    <p>
+      {props.name} {props.exerciseCount}
+    </p>
+  </>
+);
+
+const Content = (props: ContentProps): JSX.Element => (
+  <div>
+    {props.parts.map((part, idx) => (<Part key={idx} name={part.name} exerciseCount={part.exerciseCount} />))}
+  </div>
+);
+
+const Total = (props : ContentProps) => (
+  <>
+    <p>
+      Number of exercises{" "}
+      {props.parts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+    </p>
+  </>
+);
+
+const App = () => {
+  const courseName = "Half Stack application development";
+  const courseParts = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7,
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14,
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header name={courseName} />
+      <Content parts={courseParts} />
+      <Total parts={courseParts} />
     </div>
   );
-}
+};
 
 export default App;
