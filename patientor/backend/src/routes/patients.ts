3e94b5cd67a46bd258ddc1b274907991c539f4ec
leebissessar5 <leebissessar@gmail.com>
2023-06-24 20:13:44 -0400

    complete ex9.12

--- patientor/backend/src/routes/patients.ts
+++ patientor/backend/src/routes/patients.ts
@@ -1,12 +1,27 @@
 import express from 'express';
 import patientsService from '../services/patientsService';
-import { NonSensitivePatientEntry } from '../types';
+import { NonSensitivePatientEntry, Gender } from '../types';
 
 const router = express.Router();
 
 router.get('/', (_req, res) => {
     const data: NonSensitivePatientEntry[] = patientsService.getNonSensitiveEntries();
     res.json(data);
+});
+
+router.post('/', (req, res) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const { name, dateOfBirth, ssn, gender, occupation } = req.body;
+
+    const addedEntry = patientsService.addEntry(
+        name as string, 
+        dateOfBirth as string, 
+        ssn as string, 
+        gender as Gender, 
+        occupation as string
+    );
+
+    res.json(addedEntry);
 });
 
 export default router;
