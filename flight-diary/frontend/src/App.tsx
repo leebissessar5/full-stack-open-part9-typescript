efed6adfb4f20259c0632cb5954c2b352286c41b
leebissessar5 <leebissessar@gmail.com>
2023-06-27 21:34:48 -0400

    complete ex9.18

--- flight-diary/frontend/src/App.tsx
+++ flight-diary/frontend/src/App.tsx
@@ -1,32 +1,41 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { getAllDiaries, createDiary } from './diaryService';
 import { DiaryEntry, NewDiaryEntry, Weather, Visibility } from './types';
 
 const  App = () => {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([{} as DiaryEntry]);
   const [newDiary, setNewDiary] = useState<NewDiaryEntry>({} as NewDiaryEntry);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    axios.get<DiaryEntry[]>("http://localhost:3001/api/diaries").then((response) => {
-      setDiaries(response.data);
+    getAllDiaries().then((data) => {
+      setDiaries(data);
     });
   }, []);
 
 
   const diaryCreation = (event: React.SyntheticEvent) => {
     event.preventDefault();
-    axios
-      .post<DiaryEntry>("http://localhost:3001/api/diaries", { ...newDiary })
-      .then((response) => {
-        setDiaries(diaries.concat(response.data));
-      });
-    
-    setNewDiary({} as NewDiaryEntry);
+    createDiary(newDiary)
+      .then((data) => {
+        setDiaries(diaries.concat(data));
+        setNewDiary({} as NewDiaryEntry);
+        setError(null);
+      })
+      .catch((error) => {
+        if (axios.isAxiosError(error)) {
+          setError(error.response?.data as string);
+        } else {
+          setError("An error occurred while creating the diary entry.");
+        }
+      })
   };
 
   return (
     <div className="App">
       <h2>Add new entry</h2>
+      {error && <div style={{color: "red", }}>{error}</div>}
       <form onSubmit={diaryCreation}>
         <div>
           date
@@ -80,13 +89,14 @@ const  App = () => {
       </form>
       <h2>Diary entries</h2>
       <ul style={{ margin: 0, padding: 0 }}>
-        {diaries && diaries.map((diary) => (
-          <div key={diary.id}>
-            <h3>{diary.date}</h3>
-            <div>weather: {diary.weather}</div>
-            <div>visibility: {diary.visibility}</div>
-          </div>
-        ))}
+        {diaries &&
+          diaries.map((diary) => (
+            <div key={diary.id}>
+              <h3>{diary.date}</h3>
+              <div>weather: {diary.weather}</div>
+              <div>visibility: {diary.visibility}</div>
+            </div>
+          ))}
       </ul>
     </div>
   );
